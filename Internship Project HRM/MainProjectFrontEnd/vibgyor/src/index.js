16cfd25041935a7aef85ea309da1342f543b2644
hitesh <hiteshkukaramprajapati@gmail.com>
2025-05-02 12:41:27 +0530

    requirement.txt for django

--- Internship Project HRM/MainProjectFrontEnd/vibgyor/src/index.js
+++ Internship Project HRM/MainProjectFrontEnd/vibgyor/src/index.js
@@ -23,15 +23,47 @@ import "./index.css"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { useEffect, useState } from "react";
+import api from "./services/api";
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(
-  <React.StrictMode>
-    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+const Root = () => {
+  const [clientId, setClientId] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    api
+      .post("/google-client-id/")
+      .then((res) => {
+        setClientId(res.data.GOOGLE_OAUTH_CLIENT_ID);
+      })
+      .catch((err) => {
+        console.error("Failed to fetch Google Client ID:", err);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>; // or a spinner
+  }
+
+  if (!clientId) {
+    return <div>Failed to load Google Client ID.</div>;
+  }
+
+  return (
+    <GoogleOAuthProvider clientId={clientId}>
       <App />
     </GoogleOAuthProvider>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Root />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
